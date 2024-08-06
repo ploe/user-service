10c2822f0acff51d470018659db9e7158facbfb9
Myke Atkinson <ploe@hotmail.co.uk>
2024-08-06 16:56:55 +0100

    feature-post-users: TestCorrectPostUser in package "user-service/http" started #1

--- http/http_test.go
+++ http/http_test.go
@@ -1,1 +1,25 @@
 package http
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestCorrectPostUser(t *testing.T) {
+	req := map[string]string{
+		"country":    "UK",
+		"email":      "alice@bob.com",
+		"first_name": "Alice",
+		"last_name":  "Bob",
+		"nickname":   "AB123",
+		"password":   "f6b7e19e0d867de6c0391879050e8297165728d89d7c4e9e8839972b356c4d9d",
+	}
+
+	body, err := json.Marshal(req)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	fmt.Println(string(body))
+}
