d351249215e751215cead31730b030753475b3bf
Myke Atkinson <ploe@hotmail.co.uk>
2024-08-10 10:36:45 +0100

    feature-patch-users: TestPatchModifiesUpdatedAt added #6

--- http/http_test.go
+++ http/http_test.go
@@ -13,6 +13,108 @@ import (
 )
 
 /*
+TestPatchModifiesUpdatedAt: Given I have created a User and modified
+them with PATCH when I call the GET method then the User's created_at
+and updated_at attributes will be no longer be the same.
+*/
+func TestPatchModifiesUpdatedAt(t *testing.T) {
+	/* create user */
+
+	us, err := NewUserService()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	debut := map[string]string{
+		"country":    "UK",
+		"email":      "alice@bob.com",
+		"first_name": "Alice",
+		"last_name":  "Bob",
+		"nickname":   "AB123",
+		"password":   "f6b7e19e0d867de6c0391879050e8297165728d89d7c4e9e8839972b356c4d9d",
+	}
+
+	post_body, err := json.Marshal(debut)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	post_req, err := http.NewRequest("POST", "/users", bytes.NewReader(post_body))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	us.ServeHTTP(httptest.NewRecorder(), post_req)
+
+	created_get_req, err := http.NewRequest("GET", "/users", nil)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	/* after created get the user */
+
+	created_get_resp := httptest.NewRecorder()
+	us.ServeHTTP(created_get_resp, created_get_req)
+
+	created_get_body := []map[string]string{}
+
+	err = json.NewDecoder(created_get_resp.Body).Decode(&created_get_body)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	debut_updated_at := created_get_body[0]["updated_at"]
+
+	id := created_get_body[0]["id"]
+	url := fmt.Sprintf("/users/%s", id)
+
+	finale := map[string]string{
+		"country":    "USA",
+		"email":      "ken@bob.com",
+		"first_name": "Ken",
+		"last_name":  "Thompson",
+		"nickname":   "ken",
+		"password":   "b3bb4cd67f11e1f6350a5792c8a0f91c2e7920ab93ccd7e964d97d79ad9f8270",
+	}
+
+	patch_body, err := json.Marshal(&finale)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	patch_req, err := http.NewRequest("PATCH", url, bytes.NewReader(patch_body))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	patch_resp := httptest.NewRecorder()
+	us.ServeHTTP(patch_resp, patch_req)
+
+	/* after patch get users */
+
+	patched_get_req, err := http.NewRequest("GET", "/users", nil)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	patched_get_resp := httptest.NewRecorder()
+	us.ServeHTTP(patched_get_resp, patched_get_req)
+
+	patched_get_body := []map[string]string{}
+
+	err = json.NewDecoder(patched_get_resp.Body).Decode(&patched_get_body)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	finale_updated_at := patched_get_body[0]["updated_at"]
+
+	if debut_updated_at == finale_updated_at {
+		t.Fatalf("expected updated_at attribute to be modified but both were %q", debut_updated_at)
+	}
+}
+
+/*
 TestPatchModifiesAttributes: Given I have created a User and
 modified any of the following attributes: country, email, first_name,
 last_name, nickname and password when I call the GET method then the
